24e4390b5dec23e9984da02e9a6444326d35f7a4
Trevor Richards <trev@trevdev.ca>
2020-04-29 16:02:36 -0700

    refactor unlock/refresh

--- fass-desktop.py
+++ fass-desktop.py
@@ -30,15 +30,6 @@ class Fassword(QWidget):
         self.unlocked = False
         self.initUI()
 
-    def unlock(self):
-        self.verify_master = VerifyMaster(
-            self.data['key'],
-            self.data['master']
-        )
-
-        self.verify_master.unlock_signal.connect(self.unlock_storage)
-        self.verify_master.initUI()
-
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(*self.position, *self.dimens)
@@ -47,34 +38,40 @@ class Fassword(QWidget):
             self.create_password.initUI(master=True)
             self.create_password.password_signal.connect(self.new_master)
         else:
-            self.unlock()
+            self.verify_master = VerifyMaster(
+                self.data['key'],
+                self.data['master']
+            )
+            self.verify_master.unlock_signal.connect(self.unlock_storage)
+            self.verify_master.initUI()
 
     def new_master(self, password):
         self.create_password.hide()
         master, key = encrypt(password)
         save_data(create_storage(master, key))
         self.refresh()
+        self.show()
 
     def refresh(self):
         self.data = load_data()
         columns = QGridLayout()
-        left_side = QVBoxLayout()
-        right_side = QVBoxLayout()
+        top = QVBoxLayout()
+        bottom = QVBoxLayout()
 
         for entry in self.data['entry']:
             self.entries.addItem(QListWidgetItem(entry))
 
-        left_label = QLabel('Entries')
-        left_side.addWidget(left_label)
-        left_side.addWidget(self.entries)
-        left_side.addStretch(1)
+        top_label = QLabel('Entries')
+        top.addWidget(top_label)
+        top.addWidget(self.entries)
+        top.addStretch(1)
 
-        right_label = QLabel('Options')
-        right_side.addWidget(right_label)
-        right_side.addStretch(1)
+        bottom_label = QLabel('Options')
+        bottom.addWidget(bottom_label)
+        bottom.addStretch(1)
 
-        columns.addLayout(left_side, 0, 0)
-        columns.addLayout(right_side, 1, 0)
+        columns.addLayout(top, 0, 0)
+        columns.addLayout(bottom, 1, 0)
         self.setLayout(columns)
 
     def unlock_storage(self, unlock):
